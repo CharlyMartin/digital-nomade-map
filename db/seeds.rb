10cf729a165b8e8107f15016e587fa1ed44e0272
Charly MARTIN <charly.martin.1207@gmail.com>
2017-01-16 19:06:08 +0100

    new home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@
 
 # puts "Creating a nomad for Paris"
 # Nomad.create first_name: 'Paul', phone: '0', address: '41 Rue Réaumur', zip_code: 75011, city: 'Paris', country: 'FR'
+
+Nomad.destroy_all
+
+Nomad.create()
+puts 'nomad created'
