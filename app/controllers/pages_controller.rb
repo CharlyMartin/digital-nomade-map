c7e23943991407484bba1262a7f9e2d6f8dc0748
Charly MARTIN <charly.martin.1207@gmail.com>
2017-01-18 16:05:49 +0100

    home page WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_nomad!, only: [:home]
+  skip_before_action :authenticate_nomad!, only: [:home, :nomad_around]
 
   def home
+  end
+
+  def nomad_around
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   put 'api/update'
 
   root to: 'pages#home'
+  get 'nomad_around', to: 'pages#nomad_around', as: "nomad_around"
 
   devise_for :nomads
 
