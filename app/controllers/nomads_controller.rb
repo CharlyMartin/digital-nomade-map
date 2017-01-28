a172edde79885b6c16d01288fd41b2b81c9b54fe
Charly MARTIN <charly.martin.1207@gmail.com>
2017-01-28 19:22:27 +0100

    only index in nomads controller + custom images for markers with new method

--- app/controllers/nomads_controller.rb
+++ app/controllers/nomads_controller.rb
@@ -4,42 +4,15 @@ class NomadsController < ApplicationController
   def index
     @nomads = Nomad.all.order(created_at: :desc).where.not(latitude: nil, longitude: nil)
 
-    @hash = Gmaps4rails.build_markers(@nomads) do |nomad, marker|
+    @nomads_location = Gmaps4rails.build_markers(@nomads) do |nomad, marker|
       marker.lat nomad.latitude
       marker.lng nomad.longitude
+      markers_picture(marker, nomad)
       marker.infowindow render_to_string(partial: "/nomads/map_box", locals: { nomad: nomad })
     end
-  end
 
-  def new
-    @nomad = Nomad.new
-  end
+    @center_point = {lat: current_nomad.latitude, lng: current_nomad.longitude}
 
-  def create
-    @nomad = Nomad.new(nomad_params)
-    if @nomad.save
-      redirect_to root_path, notice: 'You are now on the nomad map :)'
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @nomad = Nomad.find(params[:id])
-  end
-
-  def update
-    @nomad = Nomad.find(params[:id])
-    if @nomad.update(nomad_params)
-      redirect_to root_path, notice: 'Your nomad profile has been update :)'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @nomad = Nomad.find(params[:id])
-    @nomad.destroy
   end
 
   private
@@ -48,4 +21,23 @@ class NomadsController < ApplicationController
     params.require(:nomad).permit(:first_name, :last_name, :phone, :facebook, :email,
                                   :address, :zip_code, :city, :country)
   end
+
+  def markers_picture(marker, user)
+    # https://github.com/apneadiving/Google-Maps-for-Rails/wiki/Markers
+
+    if user == current_nomad
+      marker.picture({
+        url:     'https://unsplash.it/400/400/?random',
+        width:   30,
+        height:  30
+      })
+    else
+      marker.picture({
+        url:     'https://unsplash.it/400/400/?random',
+        width:   50,
+        height:  50
+      })
+    end
+  end
+
 end
