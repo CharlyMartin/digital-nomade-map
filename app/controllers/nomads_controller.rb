981bcc367ce79b4bd512d2bb67c8d2b7144eaff8
Charly MARTIN <charly.martin.1207@gmail.com>
2017-02-16 10:49:40 +0100

    navbar edit section + clean nomad controller

--- app/controllers/nomads_controller.rb
+++ app/controllers/nomads_controller.rb
@@ -1,4 +1,5 @@
 class NomadsController < ApplicationController
+  before_action :set_nomad, only: [:edit_info, :edit_location, :update]
 
   def index
     @nomads = Nomad.all.order(created_at: :desc).where.not(latitude: nil, longitude: nil)
@@ -17,15 +18,12 @@ class NomadsController < ApplicationController
   end
 
   def edit_info
-    @nomad = Nomad.find(params[:id])
   end
 
   def edit_location
-    @nomad = Nomad.find(params[:id])
   end
 
   def update
-    @nomad = Nomad.find(params[:id])
     if @nomad.update(nomad_params)
       redirect_to nomads_path, notice: "Successfully updaded"
     else
@@ -34,6 +32,10 @@ class NomadsController < ApplicationController
   end
 
   private
+
+  def set_nomad
+    @nomad = Nomad.find(params[:id])
+  end
 
   def nomad_params
     params.require(:nomad).permit(:first_name, :last_name, :phone, :facebook, :email,
