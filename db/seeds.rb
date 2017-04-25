baa7f75101c94e23d31a8d1adb81dc6bd981a274
Charly MARTIN <charly.martin.1207@gmail.com>
2017-04-25 14:59:54 +0200

    Clustering not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,20 @@
-# puts "Destroying all nomads first..."
-# Nomad.destroy_all
-
-# puts "Creating a nomad for Paris"
-# Nomad.create first_name: 'Paul', phone: '0', address: '41 Rue Réaumur', zip_code: 75011, city: 'Paris', country: 'FR'
+# comment the vaidation for address in order to make in work
 
 # Nomad.destroy_all
+count = 0
 
-Nomad.create(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  email: Faker::Internet.email,
-  password: Faker::Hipster.sentence(2),
-  city: Faker::Address.city,
-  country: Faker::Address.country
+30.times do
+  Nomad.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Crypto.md5,
+    # address: Faker::Address.street_address,
+    # zip_code: Faker::Address.postcode,
+    # city: Faker::Address.city,
+    # country: Faker::Address.country,
+    latitude: rand(-90.0...90.0),
+    longitude: rand(-180.0...180.0)
   )
-puts 'nomad created'
+  puts count = count + 1
+end
