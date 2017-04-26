c609ca12c1b149b2eba98ef4c203007e64f206e9
Charly MARTIN <charly.martin.1207@gmail.com>
2017-04-26 13:59:56 +0200

    cluster images in subfolder

--- app/assets/javascripts/markerclusterer.js
+++ app/assets/javascripts/markerclusterer.js
@@ -198,7 +198,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = "assets/m";
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = "assets/marker-clusters/m";
 
 
 /**
