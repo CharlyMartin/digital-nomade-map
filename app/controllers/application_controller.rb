652b62468e0679c652ff231b16af230866c1c0ef
Charly MARTIN <charly.martin.1207@gmail.com>
2017-04-29 17:08:18 +0200

    i18n working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_nomad!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def configure_permitted_parameters
     devise_params = [:first_name, :last_name, :address, :zip_code, :city, :country]
 
     devise_parameter_sanitizer.permit(:sign_up, keys: devise_params)
     devise_parameter_sanitizer.permit(:account_update, keys: devise_params)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 
   # nomad#index after authentication
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   put 'api/update'
-
-  root to: 'pages#home'
-  get 'nomads_around', to: 'pages#nomads_around', as: 'nomads_around'
-  get 'misson', to: 'pages#mission', as: 'mission'
-  get 'about', to: 'pages#about', as: "about"
-
   devise_for :nomads
 
-  resources :nomads, only: %i(index show update)
-  get 'nomads/:id/edit_info', to: 'nomads#edit_info', as: 'edit_info'
-  get 'nomads/:id/edit_location', to: 'nomads#edit_location', as: 'edit_location'
+  scope '(:locale)', locale: /en|fr/ do
+    root to: 'pages#home'
+    get 'nomads_around', to: 'pages#nomads_around', as: 'nomads_around'
+    get 'misson', to: 'pages#mission', as: 'mission'
+    get 'about', to: 'pages#about', as: "about"
+
+    resources :nomads, only: %i(index show update)
+    get 'nomads/:id/edit_info', to: 'nomads#edit_info', as: 'edit_info'
+    get 'nomads/:id/edit_location', to: 'nomads#edit_location', as: 'edit_location'
+  end
 end
