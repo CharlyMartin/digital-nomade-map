9731e2e0fee4df7c36e6751de2ab1db020c3dfe3
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-13 11:26:40 +1000

    Simplifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  put 'api/update'
   devise_for :nomads, controllers: { omniauth_callbacks: 'nomads/omniauth_callbacks' }
 
-  # scope '(:locale)', locale: /en|fr/ do
-    root to: 'pages#home'
+  root to: 'pages#home'
+  get 'nomads_around', to: 'pages#nomads_around', as: 'nomads_around'
+  get 'misson', to: 'pages#mission', as: 'mission'
+  get 'privacy', to: 'pages#privacy', as: 'privacy'
 
-    get 'nomads_around', to: 'pages#nomads_around', as: 'nomads_around'
-    get 'misson', to: 'pages#mission', as: 'mission'
-    get 'privacy', to: 'pages#privacy', as: 'privacy'
+  resources :nomads, only: %i(index show edit update)
 
-    resources :nomads, only: %i(index show update)
-    get 'nomads/:id/edit', to: 'nomads#edit', as: 'edit'
-  # end
+
+  put 'api/update'
 end
