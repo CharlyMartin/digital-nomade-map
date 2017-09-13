573b8ad7435ec03dd38b01165bb8cec9719433fb
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-13 11:21:59 +1000

    no longer using namespace for locale. English as only lang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.default_locale
+    # I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   put 'api/update'
   devise_for :nomads, controllers: { omniauth_callbacks: 'nomads/omniauth_callbacks' }
 
-  scope '(:locale)', locale: /en|fr/ do
+  # scope '(:locale)', locale: /en|fr/ do
     root to: 'pages#home'
 
     get 'nomads_around', to: 'pages#nomads_around', as: 'nomads_around'
     get 'misson', to: 'pages#mission', as: 'mission'
-    get 'about', to: 'pages#about', as: "about"
     get 'privacy', to: 'pages#privacy', as: 'privacy'
 
     resources :nomads, only: %i(index show update)
     get 'nomads/:id/edit', to: 'nomads#edit', as: 'edit'
-    # get 'nomads/:id/edit_location', to: 'nomads#edit_location', as: 'edit_location'
-  end
+  # end
 end
