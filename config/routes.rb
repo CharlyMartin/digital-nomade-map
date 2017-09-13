f271803a5b633e2bfbdde6922855ff28eda1814f
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-13 11:36:18 +1000

    show and update as API routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
 
   resources :nomads, only: %i(index show edit update)
 
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get 'nomads/:email', to: 'nomads#show', as: 'api_v1_nomad'
+      post 'nomads/:email', to: 'nomads#update', , as: 'api_v1_nomad'
+    end
+  end
 
   put 'api/update'
 end
