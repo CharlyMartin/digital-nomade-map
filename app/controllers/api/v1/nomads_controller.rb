439fdf499375bd2ec47832cf1a9b6752b99f6ea2
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-19 09:32:17 +1000

    migrating into API::V1 controller

--- app/controllers/api/v1/nomads_controller.rb
+++ app/controllers/api/v1/nomads_controller.rb
@@ -1,29 +1,33 @@
 class Api::V1::NomadsController < Api::V1::BaseController
+  require "time"
+
   def index
     @nomads = Nomad.all
   end
 
   def show
-    @nomad = Nomad.find_by_username(params[:id])
+    @nomad = Nomad.find_by(username: params[:id])
   end
 
   def update
+    if @nomad = Nomad.find_by(username: params[:id])
+      @nomad.update(
+        latitude: api_params[:latitude],
+        longitude: api_params[:longitude],
+        last_chrome_update_date: Time.zone.now
+      )
+      render json: @nomad
+    end
+  end
+
+  private
+
+  def nomad_params
+    params.require(:nomad).permit(:latitude, :longitude, :last_chrome_update_date)
   end
 end
 
 
 # protect_from_forgery with: :null_session
-# require "time"
 
-# def update
-#   if @nomad = Nomad.find_by(email: api_params[:email])
-#     @nomad.update(email: api_params[:email], latitude: api_params[:latitude], longitude: api_params[:longitude], last_chrome_update_date: Time.zone.now)
-#   end
-#   render json: @nomad
-# end
 
-# private
-
-# def api_params
-#   params.permit(:email, :latitude, :longitude, :api, :last_chrome_update_date)
-# end
