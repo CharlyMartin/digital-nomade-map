71b6b993008086b0085bdeb4d96c3db9f4a2e635
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-19 09:20:25 +1000

    deleting previous api controller

--- app/controllers/api/v1/nomads_controller.rb
+++ app/controllers/api/v1/nomads_controller.rb
@@ -10,3 +10,20 @@ class Api::V1::NomadsController < Api::V1::BaseController
   def update
   end
 end
+
+
+# protect_from_forgery with: :null_session
+# require "time"
+
+# def update
+#   if @nomad = Nomad.find_by(email: api_params[:email])
+#     @nomad.update(email: api_params[:email], latitude: api_params[:latitude], longitude: api_params[:longitude], last_chrome_update_date: Time.zone.now)
+#   end
+#   render json: @nomad
+# end
+
+# private
+
+# def api_params
+#   params.permit(:email, :latitude, :longitude, :api, :last_chrome_update_date)
+# end
