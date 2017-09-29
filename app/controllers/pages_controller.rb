c42b5bbdaeff1f851015cca7a46345c4b4844e7d
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-29 15:08:49 +1000

    page for auto-update done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_nomad!, only: [:home, :nomads_around, :about, :auto_update, :privacy]
+  skip_before_action :authenticate_nomad!, only: [:home, :nomads_around, :about, :privacy]
 
   def home
     random_nomads = Nomad.order("RANDOM()").limit(15)
